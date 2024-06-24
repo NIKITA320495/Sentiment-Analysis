4401cdc4ba3ce7c2fc54050bfa6072c5f039fd17
Nikita <nikitababb036@gmail.com>
2024-06-24 23:46:17 +0530

    added nltk packages

--- download_nltk_resources.py
+++ download_nltk_resources.py
@@ -1,2 +1,10 @@
+
+import os
 import nltk
-nltk.download('punkt')
+
+# Set NLTK_DATA environment variable
+os.environ['NLTK_DATA'] = 'C:\\Users\\Nikita\\AppData\\Roaming\\nltk_data'
+
+# Download NLTK resources
+nltk.download('stopwords', download_dir=os.environ['NLTK_DATA'])
+nltk.download('punkt', download_dir=os.environ['NLTK_DATA'])
