6f9e2309b8245589520c442bb64bb7adfc00ca7a
Nikita <nikitababb036@gmail.com>
2024-06-24 23:52:37 +0530

    added nltk packages

--- download_nltk_resources.py
+++ download_nltk_resources.py
@@ -1,10 +1,16 @@
-
 import os
 import nltk
 
 # Set NLTK_DATA environment variable
 os.environ['NLTK_DATA'] = 'C:\\Users\\Nikita\\AppData\\Roaming\\nltk_data'
 
+# Ensure the NLTK_DATA directory exists
+if not os.path.exists(os.environ['NLTK_DATA']):
+    os.makedirs(os.environ['NLTK_DATA'])
+
 # Download NLTK resources
-nltk.download('stopwords', download_dir=os.environ['NLTK_DATA'])
-nltk.download('punkt', download_dir=os.environ['NLTK_DATA'])
+try:
+    nltk.download('stopwords', download_dir=os.environ['NLTK_DATA'])
+    nltk.download('punkt', download_dir=os.environ['NLTK_DATA'])
+except Exception as e:
+    print("Error downloading NLTK resources:", e)
