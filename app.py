e45d4d44c23b07aff9f7a16978341629ae0a5e4a
Nikita <nikitababb036@gmail.com>
2024-08-30 17:15:57 +0530

    punkt

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 import nltk
 import os
-
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 # Define your NLTK data path explicitly
 custom_nltk_data_path = r'C:\Users\Nikita\nltk_data'
 # Ensure the directory exists, create it if necessary
